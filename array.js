1589612cedd660ac90efc78e34b9b18f4eccd44a
alexe4540 <alexeibaton0@gmail.com>
2019-12-01 11:41:57 +0200

    feat(app) Add module array.js

---
+++ array.js
@@ -0,0 +1,5 @@
+const arrayCounter = function(array){
+    return `В массиве находится ${array.length} элементов!`;
+};
+
+module.exports = arrayCounter;
--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log("Hello, World!");
+const array = require('./array');
+
+const arr = [3,6,1,5,6,78,4];
+
+console.log(array(arr));
