39bb335e17e754361d3566c9d72980f299c9af8c
alexe4540 <alexeibaton0@gmail.com>
2019-12-01 19:29:09 +0200

    feat(things.js) change array.js to things.js. Add myltiply function

---
+++ things.js
@@ -0,0 +1,9 @@
+module.exports.arrayCounter = function(array) {
+  return `В массиве находится ${array.length} элементов!`;
+};
+
+module.exports.mutiply = function(x, y) {
+  return `${x} * ${y} = ${x * y}`;
+};
+
+module.exports.someValue = 228;
