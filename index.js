c593f9f316654240d06ed22c6c59a7ad61a8326d
alexe4540 <alexeibaton0@gmail.com>
2019-12-08 15:16:12 +0200

    feat(app) Server with index.html

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-const fs = require('fs');
-const myReadShort = fs.createReadStream(__dirname + '/article.txt');
-const myWriteShort = fs.createWriteStream(__dirname + '/news.txt');
+const fs = require("fs");
 
-myReadShort.on('data', function (chunk) {
-    console.log(`New data write: \n\n`);
-    myWriteShort.write(chunk);
-})
+const http = require("http");
+const server = http.createServer(function(req, res) {
+  console.log("URL: " + req.url);
+  res.writeHead(200, { 'Content-Type': "text/html; charset=utf-8" });
+  const myReadShort = fs.createReadStream(__dirname + "/index.html");
+
+  myReadShort.pipe(res);
+});
+
+server.listen(3000, "localhost");
+console.log("Server run on port 3000");
