339c449dfa4ef5513cf1ff6292d471e38537f8a2
alexe4540 <alexeibaton0@gmail.com>
2019-12-08 15:37:12 +0200

    feat(app) Add routing and pages index, about and 404

--- index.js
+++ index.js
@@ -3,10 +3,22 @@ const fs = require("fs");
 const http = require("http");
 const server = http.createServer(function(req, res) {
   console.log("URL: " + req.url);
-  res.writeHead(200, { 'Content-Type': "text/html; charset=utf-8" });
-  const myReadShort = fs.createReadStream(__dirname + "/index.html");
+  if (req.url === "/index" || req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+    const myReadShort = fs.createReadStream(__dirname + "/index.html");
 
-  myReadShort.pipe(res);
+    myReadShort.pipe(res);
+  } else if (req.url === "/about") {
+    res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+    const myReadShort = fs.createReadStream(__dirname + "/about.html");
+
+    myReadShort.pipe(res);
+  } else{
+    res.writeHead(404, { "Content-Type": "text/html; charset=utf-8" });
+    const myReadShort = fs.createReadStream(__dirname + "/404.html");
+
+    myReadShort.pipe(res);
+  }
 });
 
 server.listen(3000, "localhost");
