a99b233cb07ac33f55bd8a89a04c98a8a6581324
alexe4540 <alexeibaton0@gmail.com>
2019-12-10 22:03:09 +0200

    feat(app) Create and use static file

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const app = express();
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
+
+app.use('/public', express.static('public'));
 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
+    res.render('index');
 });
 
 app.get('/about', function (req, res) {
-    res.sendFile(__dirname + '/about.html');
+    res.render('about');
 });
 
-app.get('/about/:id', function (req, res) {
+app.get('/news/:id', function (req, res) {
     let obj = {title: "news", id: "25", par : ['paragraphs', 'first', 'numbers', 569, true]}
     res.render('news', {newsId : req.params.id, randomNumber: Math.random(), obj : obj});
 });
